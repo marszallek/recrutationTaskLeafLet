f8c5b492f86087395191a33b26150dc59e3d61a1
Daniel Marszalek <marszallek@vp.pl>
2019-12-08 00:24:53 +0100

    Fixing issues

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = new Vue({
         map: null,
         tileLayer: null,
         markers: [],
-        index: 1
+        index: 0,
+        coordinates: [],
+        hemisphere: 'N',
+        hemiSphere: 'E'
     },
     mounted() {
         this.initMap();
@@ -13,7 +16,7 @@ const app = new Vue({
         initMap() {
             this.map = L.map('map').setView([54.51, 18.53], 12),
             this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
                 id: 'mapbox.streets',
                 accessToken: 'pk.eyJ1IjoibWFyc3phbGxlayIsImEiOiJjanV0d3pvcngwOWJ2M3luNGR4cXZxMmR4In0.xuzEdqouG0Ik7kdgG4oLRQ'
@@ -23,10 +26,17 @@ const app = new Vue({
         },
         addMarker(event) {
             let lat = this.map.mouseEventToLatLng(event).lat;
+            let latDegree = parseInt(lat);
+            let latMinutes = Math.round((lat - latDegree)*6000)/100;
             let long = this.map.mouseEventToLatLng(event).lng;
+            let longDegree = parseInt(long);
+            let longMinutes = Math.round((long - longDegree)*6000)/100;
+            (lat < 0) ? hemisphere = 'S' : hemisphere = 'N';
+            (long < 0) ? hemiSphere = 'W' : hemiSphere = 'E';
             this.markers.push(
-                L.marker([lat, long], {draggable: true, title: `${this.index++}`}).addTo(this.map),    
-            );
+                L.marker([lat, long], {draggable: true, title: `${this.index++}`}).addTo(this.map),   
+            ),
+            this.coordinates.push([this.index,`${hemisphere} ${latDegree}° ${latMinutes}"`, `${hemiSphere} ${longDegree}° ${longMinutes}"`])
         },
     },
 });
